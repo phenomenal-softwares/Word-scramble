0cec0f664c17c37a02d6f8f38978115b6f15b24a
Phenomenal <115654413+phenomenal-softwares@users.noreply.github.com>
2023-06-05 16:31:51 +0100

    Add files via upload

---
+++ words.js
@@ -0,0 +1,33 @@
+export let wordsJSON = [
+    {
+        'word':`limit`,
+        'hint': `final, utmost, or furthest boundary or point as to extent, amount, continuance, procedure`
+    },{
+        'word':`enrichment`,
+        'hint': `an act of enriching`
+    },{
+        'word':`cordless`,
+        'hint': `lacking a cord`
+    },{
+        'word':`vertical`,
+        'hint': `an upright structure.`
+    },{
+        'word':`cooperate`,
+        'hint': `assist someone or comply with their requests.`
+    },{
+        'word':`misplace`,
+        'hint': `put in the wrong place and lose temporarily`
+    },{
+        'word':`shrink`,
+        'hint': `become or make smaller in size or amount`
+    },{
+        'word':`private`,
+        'hint': `belonging to or for the use of one particular person or group of people only`
+    },{
+        'word':`scramble`,
+        'hint': `a disordered mixture of things.`
+    },{
+        'word':`retain`,
+        'hint': `not abolish or alter; maintain`
+    }
+]
